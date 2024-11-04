1e84c81d3ad2be0d7d32a4463b253f4bf625c3eb
xblack <vatsal.bajpai05@gmail.com>
2024-11-04 14:38:34 +0530

    add pre deploy script run

---
+++ dummy.sh
@@ -0,0 +1,9 @@
+#! /bin/bash
+
+echo "Running dummy script"
+
+# Generate random number between 1-10000
+RANDOM_NUM=$((1 + RANDOM % 10000))
+
+# Output to random-number.txt in current directory
+echo $RANDOM_NUM > random-number.txt
